ab8fed5dd2d24938b827e4b537a896eb545f9040
hemengyang <hmy0119@gmail.com>
2020-11-09 12:50:26 +0800

    修改服务器的域名

--- acme.sh/issue-and-deploy-ssl-key.sh
+++ acme.sh/issue-and-deploy-ssl-key.sh
@@ -1,11 +1,11 @@
 docker exec \
     -e DP_Id="your_id" \
     -e DP_Key="your_key"  \
-    acme.sh --issue --dns dns_dp -d hehome.xyz -d *.hehome.xyz
+    acme.sh --issue --dns dns_dp -d hemengyang.tk -d *.hemengyang.tk
 
 docker exec \
-    -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=hehome.xyz \
-    -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/hehome.xyz.key" \
+    -e DEPLOY_DOCKER_CONTAINER_LABEL=sh.acme.autoload.domain=hemengyang.tk \
+    -e DEPLOY_DOCKER_CONTAINER_KEY_FILE="/etc/nginx/ssl/hemengyang.tk.key" \
     -e DEPLOY_DOCKER_CONTAINER_FULLCHAIN_FILE="/etc/nginx/ssl/fullchain.cer" \
     -e DEPLOY_DOCKER_CONTAINER_RELOAD_CMD="service nginx force-reload" \
-    acme.sh --deploy -d hehome.xyz  --deploy-hook docker
+    acme.sh --deploy -d hemengyang.tk  --deploy-hook docker
